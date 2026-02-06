ebcf47fa10e6fda9de7b80cab1b5e61e73d61505
RabeelAhmed <rabeelsulehria3@gmail.com>
2026-02-06 21:53:29 +0500

    feat: add Google Search Console verification meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ export default function MyApp({ Component, pageProps }) {
         <meta property="og:description" content="Crafting digital experiences with MERN stack and Next.js. View projects and experience of Rabeel Ahmed." />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/profile.png" />
+        <meta name="google-site-verification" content="D7esaZKJ1Ul04XFd-cmBp0v9GZZtKtLBXLWiCH2Tlog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
